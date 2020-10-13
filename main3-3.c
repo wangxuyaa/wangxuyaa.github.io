0dc28cfc2ee1693c9cbe9107dddfb201c8f55fa4
wangxuyaa <72337082+wangxuyaa@users.noreply.github.com>
2020-10-13 15:50:22 +0800

    Add files via upload

---
+++ main3-3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x;
+    printf("请输入一个整数：");
+    scanf("%d",&x);
+    if(x<0)
+        printf("绝对值是%d。\n",-x);
+    else
+        printf("绝对值是%d。\n",x);
+    return 0;
+}
